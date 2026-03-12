333b05251daba2288a0ed2ac97c07e011135b6ac
Handroc <andricassani42069@gmail.com>
2026-03-12 10:15:02 +0100

    feat: modify user model to hash passwords using bcrypt

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -3,11 +3,13 @@ import re
 from .base_model import BaseModel
 
 class User(BaseModel):
-    def __init__(self, first_name="", last_name="", email="", is_admin=False):
+    def __init__(self, first_name="", last_name="", email="", password=None, is_admin=False):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
+        if password is not None:
+            self.password = password
         self.is_admin = is_admin
 
     @property
@@ -25,6 +27,10 @@ class User(BaseModel):
     @property
     def is_admin(self):
         return self._is_admin
+
+    @property
+    def password(self):
+        return self._password
     
     @first_name.setter
     def first_name(self, value):
@@ -63,6 +69,24 @@ class User(BaseModel):
             raise TypeError("is_admin must be a boolean")
         self._is_admin = value
 
+    @password.setter
+    def password(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Password must be a string")
+        if not value:
+            raise ValueError("Password cannot be empty")
+        self._password = value
+
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        from app import bcrypt
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        """Verifies if the provided password matches the hashed password."""
+        from app import bcrypt
+        return bcrypt.check_password_hash(self.password, password)
+
     def __str__(self):
         """Return a readable string representation of the User."""
         return f"[User] {self.first_name} {self.last_name} ({self.id})"
@@ -70,4 +94,3 @@ class User(BaseModel):
     def __repr__(self):
         """Return the official string representation of the User."""
         return f"User(id='{self.id}', email='{self.email}')"
-
