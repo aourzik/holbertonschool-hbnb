c8ffd55e5331f96c3334cf94cd38d05693e2504b
Handroc <andricassani42069@gmail.com>
2026-03-16 15:33:32 +0100

    Require review constructor fields

--- part3/hbnb/app/models/review.py
+++ part3/hbnb/app/models/review.py
@@ -3,7 +3,7 @@ from .base_model import BaseModel
 
 
 class Review(BaseModel):
-    def __init__(self, text="", rating=1, place_id="", user_id=""):
+    def __init__(self, text, rating, place_id, user_id):
         super().__init__()
         self.text = text
         self.rating = rating
