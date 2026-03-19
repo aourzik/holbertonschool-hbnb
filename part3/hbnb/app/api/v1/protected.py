000cbe1e99479bf1a6a5279f5f2c95fc99c7662e
Handroc <andricassani42069@gmail.com>
2026-03-19 09:49:58 +0100

    Remove debug logging from protected endpoint

--- part3/hbnb/app/api/v1/protected.py
+++ part3/hbnb/app/api/v1/protected.py
@@ -8,8 +8,6 @@ class ProtectedResource(Resource):
     @jwt_required()
     def get(self):
         """A protected endpoint that requires a valid JWT token"""
-        print("jwt------")
-        print(get_jwt_identity())
         current_user = get_jwt_identity()
         is_admin = get_jwt().get("is_admin", False)
         if is_admin:
